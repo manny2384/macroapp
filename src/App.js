838701967164063cd99ed2ef31744c69b13de22c
manny2384 <gonzalezeh2015@gmail.com>
2023-09-27 20:17:30 -0700

    gui styling phase 1

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import './App.css';
 function App() {
 
   const [macros, setMacros] = useState([
-    {name: 'carb', grams: 0},
-    {name: 'fat', grams: 0},
-    {name: 'protein', grams: 0}
+    {labels: 'carb', grams: 0, fill:'#19324d'},
+    {labels: 'fat', grams: 0, fill:'#FFDA83'},
+    {labels: 'protein', grams: 0, fill:'rgb(82, 24, 24)'}
   ]); // carb, fat, protein
   useEffect(()=>{
     console.log(macros);
@@ -20,9 +20,9 @@ function App() {
 
   function addMeal(){
     setMacros([
-      {name: 'carb', grams: macros[0].grams+Number(carbs)},
-      {name: 'fat', grams: macros[1].grams+Number(fats)},
-      {name: 'protein', grams: macros[2].grams+Number(proteins)}
+      {labels: 'carb', grams: macros[0].grams+Number(carbs), fill:'#19324d'},
+      {labels: 'fat', grams: macros[1].grams+Number(fats), fill:'#FFDA83'},
+      {labels: 'protein', grams: macros[2].grams+Number(proteins), fill:'rgb(82, 24, 24)'}
     ]);
     setFats(0);
     setCarbs(0);
@@ -31,9 +31,9 @@ function App() {
 
   function removeMeal(){
     setMacros([
-      {name: 'carb', grams: macros[0].grams - Number(fats) >= 0 ? macros[0].grams - Number(carbs) : 0},
-      {name: 'fat', grams: macros[1].grams - Number(fats) >= 0 ? macros[1].grams - Number(fats) : 0},
-      {name: 'protein', grams: macros[2].grams - Number(fats) >= 0 ? macros[2].grams - Number(proteins) : 0}
+      {labels: 'carb', grams: macros[0].grams - Number(fats) >= 0 ? macros[0].grams - Number(carbs) : 0, fill:'#19324d'},
+      {labels: 'fat', grams: macros[1].grams - Number(fats) >= 0 ? macros[1].grams - Number(fats) : 0, fill:'#FFDA83'},
+      {labels: 'protein', grams: macros[2].grams - Number(fats) >= 0 ? macros[2].grams - Number(proteins) : 0, fill:'rgb(82, 24, 24)'}
     ]);
 
     setFats(0);
@@ -41,29 +41,43 @@ function App() {
     setProteins(0);
   }
 
-  return (<div className="App">
+  return (<div className="App flexColumn">
 
     <div>
-      <div>
-        <label> Fats: <input type='number' placeholder='0' min='0' value={fats} onInput={e=>setFats(e.target.value)} /> </label>
-        <label> Carbs: <input type='number' placeholder='0' min='0' value={carbs} onInput={e=>setCarbs(e.target.value)} /> </label>
-        <label> Proteins: <input type='number' placeholder='0' min='0' value={proteins} onInput={e=>setProteins(e.target.value)} /> </label>
+      <div className='labels flexColumn'>
+        <div>
+          <label> Fats: <input type='number' placeholder='0' min='0' value={fats} onInput={e=>setFats(e.target.value)} /> </label>
+        </div>
+
+        <div>
+          <label> Carbs: <input type='number' placeholder='0' min='0' value={carbs} onInput={e=>setCarbs(e.target.value)} /> </label>
+        </div>
+
+        <div>
+          <label> Proteins: <input type='number' placeholder='0' min='0' value={proteins} onInput={e=>setProteins(e.target.value)} /> </label>
+        </div>
+
       </div>
-      <div>
+      <div className='flexRow'>
         <button onClick={addMeal}> Add </button>
         <button onClick={removeMeal}> Remove </button>
       </div>
     </div>
 
-    <div className='stats'>
+    <div className='stats flexColumn'>
+      <div> Nutrition Summary </div>
       <div> Carbs: {macros[0].grams}g </div>
       <div> Fats: {macros[1].grams}g </div>
       <div> Proteins: {macros[2].grams}g </div>
-      <div> Total Calorie Count: {macros[0].grams*4 + macros[1].grams*9 + macros[2].grams*4} </div>
+      <div> Calories: {macros[0].grams*4 + macros[1].grams*9 + macros[2].grams*4} </div>
       <div width={300} height={400}>
 
       </div>
     </div>
+
+    <PieChart width={200} height={200}>
+      <Pie data={macros} dataKey='grams' outerRadius={80} fill='black' />
+    </PieChart>
 
   </div>);
 }
